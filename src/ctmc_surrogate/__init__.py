82e4cda95531645008bbaa0d0c59f41fa57d4596
natsuki0720 <165759764+natsuki0720@users.noreply.github.com>
2026-02-12 16:50:49 +0900

    device不一致回避と公開APIのエクスポート整理

--- src/ctmc_surrogate/__init__.py
+++ src/ctmc_surrogate/__init__.py
@@ -1,8 +1,11 @@
 """CTMCサロゲート推定の公開版パッケージ。"""
 
+from .data import CTMCSurrogateDataset, ctmc_collate_fn
 from .models import DeepSetsVarSetsAttnRegressor, build_model
 
 __all__ = [
+    "CTMCSurrogateDataset",
+    "ctmc_collate_fn",
     "DeepSetsVarSetsAttnRegressor",
     "build_model",
 ]
